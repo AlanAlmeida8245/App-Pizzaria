27faed0ee0e30e124dc9dcc106d90d6d7462bd55
Alan Almeida <alanalmeida8245@gmail.com>
2022-12-11 17:17:54 -0300

    db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 
 if(process.env.NODE.ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://alanalmeida:82450225@cluster0.4udxzwa.mongodb.net/?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://AlanAlmeida8245:82450225@blogapp.wqa7kil.mongodb.net/?retryWrites=true&w=majority"}
 
 }else{
     module.exports = {mongoURI: "mongodb://localhost/projeto"}
